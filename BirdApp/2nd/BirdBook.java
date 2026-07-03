ade37b29bce81b27943cd5a9c3636faa49088f9d
shohei-fujiya <gentleandpowerful@gmail.com>
2026-07-03 12:39:56 +0900

    Add files via upload
    コードの更新

--- BirdApp/2nd/BirdBook.java
+++ BirdApp/2nd/BirdBook.java
@@ -16,5 +16,42 @@ public class BirdBook {
         System.out.println(lastBird.name + "(" + lastBird.place + ")");
     }
 
+    //名前検索メソッド
+    public void birdNameSearch(ArrayList<Bird> list, String keyword) {
+        boolean found = false;
+        int count = 0;
+        for(Bird birdNameRecord : list){
+            if(birdNameRecord.name.equals(keyword)){
+                System.out.println(birdNameRecord.name + "(" + birdNameRecord.place + ")");
+                found = true;
+                count++;
+            }
+        }
+        if(found == false){
+            System.out.println("該当なしです");
+        }
+        System.out.println("計" + count + "件見つかりました");
+    }
 
+    //場所検索メソッド
+    public void birdPlaceSearch(ArrayList<Bird> list, String keyword){
+        System.out.println(" ");
+        System.out.println("---------------------------------------------");
+        System.out.println("===「" + keyword + "」の検索結果===");
+        boolean found = false;
+        int count = 0;
+        for(Bird birdRecord : list){
+            if(birdRecord.place.equals(keyword)){
+                System.out.println(birdRecord.name + "(" + birdRecord.place + ")");
+                found = true;
+                count++;
+            }
+        }
+        
+        if(found == false){
+                System.out.println("該当なしです");
+            }
+
+        System.out.println("計" + count + "件見つかりました");
+    }
 }
--- BirdApp/2nd/Main.java
+++ BirdApp/2nd/Main.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Bird> birdList = new ArrayList<>();
+        BirdBook myBook = new BirdBook();
 
         //リスト
-        birdList.add(new Bird("yukihoojiro", "nishikou"));
-        birdList.add(new Bird("shimaenaga", "mihara"));
-        birdList.add(new Bird("benimashiko", "kitazono"));
-        birdList.add(new Bird("suzume", "kitazono"));
-        birdList.add(new Bird("akagera", "shouwaminami"));
-        birdList.add(new Bird("kogera", "mihara"));
-        birdList.add(new Bird("kirennjaku", "nusamai"));
-        birdList.add(new Bird("kibitaki", "unndoukouenn"));
-        birdList.add(new Bird("nobitaki", "sinnfuji"));
-        birdList.add(new Bird("hakusekirei", "mihara"));
-        birdList.add(new Bird("kawarahiwa", "shouwatyuuou"));
-        birdList.add(new Bird("kogera", "sakuragaoka"));
-        birdList.add(new Bird("shijuukara", "kitazono"));
-        birdList.add(new Bird("shijuukara", "sakuragaoka"));
-        birdList.add(new Bird("oowashi", "hoshigaura"));
-        birdList.add(new Bird("hiyodori", "mihara"));
-        birdList.add(new Bird("gojuukara", "mihara"));
-        birdList.add(new Bird("sime", "mihara"));
+        myBook.list.add(new Bird("yukihoojiro", "nishikou"));
+        myBook.list.add(new Bird("shimaenaga", "mihara"));
+        myBook.list.add(new Bird("benimashiko", "kitazono"));
+        myBook.list.add(new Bird("suzume", "kitazono"));
+        myBook.list.add(new Bird("akagera", "shouwaminami"));
+        myBook.list.add(new Bird("kogera", "mihara"));
+        myBook.list.add(new Bird("kirennjaku", "nusamai"));
+        myBook.list.add(new Bird("kibitaki", "unndoukouenn"));
+        myBook.list.add(new Bird("nobitaki", "sinnfuji"));
+        myBook.list.add(new Bird("hakusekirei", "mihara"));
+        myBook.list.add(new Bird("kawarahiwa", "shouwatyuuou"));
+        myBook.list.add(new Bird("kogera", "sakuragaoka"));
+        myBook.list.add(new Bird("shijuukara", "kitazono"));
+        myBook.list.add(new Bird("shijuukara", "sakuragaoka"));
+        myBook.list.add(new Bird("oowashi", "hoshigaura"));
+        myBook.list.add(new Bird("hiyodori", "mihara"));
+        myBook.list.add(new Bird("gojuukara", "mihara"));
+        myBook.list.add(new Bird("sime", "mihara"));
 
         //メニュー表示
         while (true) {
@@ -41,33 +40,27 @@ public class Main {
             if(inputMenu.equals("1")){
                 System.out.print("検索したい鳥の名前を入力：");
                 String nameKeyWord = scanner.nextLine();
-                Bird.birdNameSearch(birdList, nameKeyWord);
+                myBook.birdNameSearch(myBook.list, nameKeyWord);
             }else if(inputMenu.equals("2")){
                 System.out.print("検索したい場所を入力：");
                 String placeKeyWord = scanner.nextLine();
-                Bird.search(birdList, placeKeyWord);
+                myBook.birdPlaceSearch(myBook.list, placeKeyWord);
             }else if(inputMenu.equals("3")){
                 System.out.println("---鳥の情報を入力して下さい---");
                 System.out.print("鳥の名前：");
                 String inputName = scanner.nextLine();
                 System.out.print("観察場所：");
                 String inputPlace = scanner.nextLine();
-                birdList.add(new Bird(inputName, inputPlace));
+                myBook.list.add(new Bird(inputName, inputPlace));
             }else if(inputMenu.equals("4")){
-                Bird.show(birdList); 
+                myBook.show(); 
             }else if(inputMenu.equals("5")){
-                System.out.println("終了します");
+                System.out.println("お疲れさまでした。終了します");
                 break;
             }else{
                 System.out.println("1～5の番号を入力して下さい");
             }
         }
-
-
-        //入力
-        
-
-        //表示
-               
+ 
     }
 }
